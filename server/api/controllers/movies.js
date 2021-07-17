9f85db13e24fb38b21be1cabad253d5e28a509f0
MOUAD AGUIRAR <mouad.aguirar@alumni.univ-avignon.fr>
2021-07-17 17:37:19 +0100

    adding more movie's categories

--- server/api/controllers/movies.js
+++ server/api/controllers/movies.js
@@ -38,7 +38,98 @@ exports.getTopRated = async function (req,res,next)  {
 exports.getNetflixOriginals = async function (req,res,next)  {
     
     
-    fetch("https://api.themoviedb.org/3/discover/tv?page=1&with_networks=213&api_key="+process.env.API_KEY)
+    fetch("https://api.themoviedb.org/3/discover/movie?page=1&with_networks=213&api_key="+process.env.API_KEY)
+    .then(res=>res.json())
+    .then(data=>{
+        res.status(200).json(data);
+        console.log(data);
+    })
+}
+
+
+
+exports.getActionMovies = async function (req,res,next)  {
+    
+    
+    fetch("https://api.themoviedb.org/3/discover/movie?page=1&with_genres=28&api_key="+process.env.API_KEY)
+    .then(res=>res.json())
+    .then(data=>{
+        res.status(200).json(data);
+        console.log(data);
+    })
+}
+
+exports.getComedyMovies = async function (req,res,next)  {
+    
+    
+    fetch("https://api.themoviedb.org/3/discover/movie?page=1&with_genres=25&api_key="+process.env.API_KEY)
+    .then(res=>res.json())
+    .then(data=>{
+        res.status(200).json(data);
+        console.log(data);
+    })
+}
+
+
+exports.getHorrorMovies = async function (req,res,next)  {
+    
+    
+    fetch("https://api.themoviedb.org/3/discover/movie?page=1&with_genres=27&api_key="+process.env.API_KEY)
+    .then(res=>res.json())
+    .then(data=>{
+        res.status(200).json(data);
+        console.log(data);
+    })
+}
+
+exports.getAdventureMovies = async function (req,res,next)  {
+    
+    
+    fetch("https://api.themoviedb.org/3/discover/movie?page=1&with_genres=12&api_key="+process.env.API_KEY)
+    .then(res=>res.json())
+    .then(data=>{
+        res.status(200).json(data);
+        console.log(data);
+    })
+}
+
+exports.getDramaMovies = async function (req,res,next)  {
+    
+    
+    fetch("https://api.themoviedb.org/3/discover/movie?page=1&with_genres=18&api_key="+process.env.API_KEY)
+    .then(res=>res.json())
+    .then(data=>{
+        res.status(200).json(data);
+        console.log(data);
+    })
+}
+
+exports.getFamiltyMovies = async function (req,res,next)  {
+    
+    
+    fetch("https://api.themoviedb.org/3/discover/movie?page=1&with_genres=10751&api_key="+process.env.API_KEY)
+    .then(res=>res.json())
+    .then(data=>{
+        res.status(200).json(data);
+        console.log(data);
+    })
+}
+
+exports.getHistoryMovies = async function (req,res,next)  {
+    
+    
+    fetch("https://api.themoviedb.org/3/discover/movie?page=1&with_genres=36&api_key="+process.env.API_KEY)
+    .then(res=>res.json())
+    .then(data=>{
+        res.status(200).json(data);
+        console.log(data);
+    })
+}
+
+exports.getThrillerMovies = async function (req,res,next)  {
+    
+    
+    fetch("https://api.themoviedb.org/3/discover/movie?page=1&with_genres=53&api_key="+process.env.API_KEY)
     .then(res=>res.json())
     .then(data=>{
         res.status(200).json(data);
--- server/api/routes/movies/movies.js
+++ server/api/routes/movies/movies.js
@@ -5,7 +5,14 @@ const moviesController = require('../../controllers/movies')
 router.get('/',moviesController.getAll);
 
 router.get('/trending/movie/week',moviesController.getTrendingMovies);
-router.get('/movies/topRated/',moviesController.getTopRated);
-router.get('/tv/netflixOriginals',moviesController.getNetflixOriginals);
+router.get('/movie/topRated/',moviesController.getTopRated);
+router.get('/movie/netflixOriginals',moviesController.getNetflixOriginals);
+router.get('/movie/action',moviesController.getActionMovies);
+router.get('/movie/comedy',moviesController.getComedyMovies);
+router.get('/movie/adventure',moviesController.getAdventureMovies);
+router.get('/movie/drama',moviesController.getDramaMovies);
+router.get('/movie/family',moviesController.getFamiltyMovies);
+router.get('/movie/history',moviesController.getHistoryMovies);
+router.get('/movie/thriller',moviesController.getThrillerMovies);
 
 module.exports = router
